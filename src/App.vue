6ff64b061a8b96bfe0448256790625809f98601b
ffotaku <68525309+ffotaku@users.noreply.github.com>
2020-08-21 14:29:08 +0700

    enter

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,68 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <img
+      src="https://siamrath.co.th/files/styles/1140/public/img/20191127/a90b412f49339d8eaf84096587c65daeeda24c78fbb1bc7a50039b8ba31820b9.jpg?itok=PaZcvRIG"
+      class=" rounded " width="30%" height="20%"
+    />
+    <p><br /> </p>
+    
+    <HelloWorld msg="KAWAII !!" />
+    <br />
+
+    <div class="row">
+      <div class="col-4">
+        <img
+          src="https://www.benarnews.org/thai/news/TH-Prawit-allegation-01162018140112.html/180116-TH-prawit-1000.jpg/@@images/cdbe68c1-c96f-4bc8-b869-f8fc24f7c3d0.jpeg"      
+          class=" rounded "  width="70%" height="50%"        
+        />
+        <p>
+          <br />
+        </p>
+        <Button  />
+      </div>
+      <div class="col-4">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ8NcwamfTL4YYjqYj7LHa3blrr_o5JXmsZ1ReIzlEZwQ&usqp=CAU&ec=45690268"
+          class="rounded "  width="70%" height="50%"        
+        />
+        <p>
+          <br />
+        </p>
+        <Button />
+      </div>
+      <div class="col-4">
+        <img
+          src="https://thestandard.co/wp-content/uploads/2019/01/%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B8%A7%E0%B8%B4%E0%B8%95%E0%B8%A3_cover-web.jpg"
+          class="  rounded "  width="70%" height="50%"
+         
+        />
+        <p>
+          <br />
+        </p>
+        <Button />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Button from "./components/Button.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    Button,
+  },
+
+  data: function () {
+    return {
+      total: 0, //ผลรวมทั้งหมด  -->เอาไปแสดงผล {{}}
+    };
+  },
+};
 </script>
 
 <style>
---
+++ vue77/src/components/Counter.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <button @click="upFunction" class="btn btn-info">{{lable}}</button>
+    <button @click="downFunction" class="btn btn-info ml-3">{{lable}}</button>
+    <p v-bind:style="{ fontSize: fsize + 'px' }">{{ counter }}</p>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      counter: 0,
+      fsize: 50,
+    };
+  },
+
+  props: {
+    lable: String,
+  },
+
+  methods: {
+    upFunction: function () {
+      this.counter += 1;
+      this.fsize += 10;
+
+      //console.log('fsize =' +this.fsize);
+      //console.log('counter =' +this.counter);
+
+      //ส่งค่าตัวแปร counter ออกไปโดยผ่านตัวแปร cc
+      this.$emit('cc',this.counter)
+
+    },
+
+    downFunction: function () {
+      this.counter -= 1;
+      this.fsize -= 10;
+
+      console.log(this.fsize);
+      console.log(this.counter);
+    },
+  },
+};
+</script>
+
+
+<style>
+</style>
+
